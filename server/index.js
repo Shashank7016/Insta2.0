d1ad031aa665b2b511ca19b1707741386c3f9fe3
Shashank7016 <bryanhovak@gmail.com>
2024-07-04 23:40:47 +1000

    making backed for insta2.0

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+// Add this line after other app.use middleware
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
